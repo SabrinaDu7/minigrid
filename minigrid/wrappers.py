4b0ccd26fb0f44cd10d647d24823198783ab78f9
Alex Efremov <efremov.aleksei.2016@post.bio.msu.ru>
2023-04-06 16:54:22 -0400

    Changed the PO_HD wrapper

--- minigrid/wrappers.py
+++ minigrid/wrappers.py
@@ -243,39 +243,6 @@ class RGBImgPartialObsWrapper(ObservationWrapper):
         rgb_img_partial = self.get_frame(tile_size=self.tile_size, agent_pov=True)
 
         return {**obs, "image": rgb_img_partial}
-
-
-# class RGBImgPartialObsWrapper(ObservationWrapper):
-#     """
-#     Wrapper to use partially observable RGB image as observation.
-#     This can be used to have the agent to solve the gridworld in pixel space.
-#     """
-
-#     def __init__(self, env, tile_size=8):
-#         super().__init__(env)
-
-#         self.tile_size = tile_size
-
-#         obs_shape = env.observation_space['image'].shape
-#         self.observation_space.spaces['image'] = spaces.Box(
-#             low=0,
-#             high=255,
-#             shape=(obs_shape[0] * tile_size, obs_shape[1] * tile_size, 3),
-#             dtype='uint8'
-#         )
-
-#     def observation(self, obs):
-#         env = self.unwrapped
-
-#         rgb_img_partial = env.get_obs_render(
-#             obs['image'],
-#             tile_size=self.tile_size
-#         )
-
-#         return {
-#             'mission': obs['mission'],
-#             'image': rgb_img_partial
-#         }
     
     
 class RGBImgPartialObsWrapper_HD(ObservationWrapper):
@@ -303,10 +270,7 @@ class RGBImgPartialObsWrapper_HD(ObservationWrapper):
     def observation(self, obs):
         env = self.unwrapped
 
-        rgb_img_partial = env.get_obs_render(
-            obs['image'],
-            tile_size=self.tile_size
-        )
+        rgb_img_partial = self.get_frame(tile_size=self.tile_size, agent_pov=True)
 
         return {
             'mission': obs['mission'],
