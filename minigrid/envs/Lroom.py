25463b7e1e1a7a85cf6137118584abbc5e1210c1
Sabrina Du <sabrina.du1972@gmail.com>
2025-09-09 12:57:01 -0400

    Broken code: Function missing keyword argument

--- minigrid/envs/Lroom.py
+++ minigrid/envs/Lroom.py
@@ -39,7 +39,7 @@ class L_Env(MiniGridEnv):
     def _gen_mission():
         return "reach the goal"
 
-    def _gen_grid(self, width, height):
+    def _gen_grid(self, width, height, regenerate=True):
         # Create an empty grid
         self.grid = Grid(width, height)
         
